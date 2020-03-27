c5c8bbb1f2123a0c26c5a7bf017f3b13487410b0
namanjain300 <namanjain300.nj@gmail.com>
2020-03-27 07:26:42 +0530

    First Commit

---
+++ lib/coin_data.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
+
+const coinAPIURL = 'https://blockchain.info/ticker';
+String currencyUnit = 'USD';
+
+class CoinData {
+  Future getCoinData(String currencyUnit) async {
+    String requestURL = '$coinAPIURL';
+    http.Response response = await http.get(requestURL);
+
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      var lastPrice = decodedData[currencyUnit]['last'];
+      return lastPrice.toStringAsFixed(0);
+    } else {
+      print(response.statusCode);
+      throw 'Problem with the get request';
+    }
+  }
+}
